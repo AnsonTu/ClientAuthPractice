028e96870643a4a47ca994d79aae58940e1ee930
Anson Tu <anson9137@gmail.com>
2019-10-08 16:35:50 -0400

    Fixed typo

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,5 @@ import { AUTH_USER } from "./types";
 
 //formProps consists of the email and password
 export const signup = formProps => dispatch => {
-  axios.post("http://localhost3090/signup", formProps);
+  axios.post("http://localhost:3090/signup", formProps);
 };
