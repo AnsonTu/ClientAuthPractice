42e5bc4dee5c9c302d05db6bb75c91ae31b5e069
Anson Tu <anson9137@gmail.com>
2019-10-15 16:00:15 -0400

    Dispatch actions after signing up

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { AUTH_USER } from "./types";
 
 //formProps consists of the email and password
-export const signup = formProps => dispatch => {
-  axios.post("http://localhost:3090/signup", formProps);
+export const signup = formProps => async dispatch => {
+  const response = await axios.post("http://localhost:3090/signup", formProps);
+
+  dispatch({ type: AUTH_USER, payload: response.data.token });
 };
