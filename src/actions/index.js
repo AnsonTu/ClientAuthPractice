d88a7672be26b9203640a1df54b1b3c6c023f4ef
Anson Tu <anson9137@gmail.com>
2019-10-27 01:12:52 -0400

    Added sign in route and component

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,6 +21,26 @@ export const signup = (formProps, callback) => async dispatch => {
   }
 };
 
+//formProps consists of the email and password
+export const signin = (formProps, callback) => async dispatch => {
+  // Try to sign in with the credentials (formProps) provided by the user
+  // If sign in is successful, redirect the user to /feature
+  // Otherwise, display an error message
+  try {
+    const response = await axios.post(
+      "http://localhost:3090/signin",
+      formProps
+    );
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    // Persist the login state in the browser
+    localStorage.setItem("token", response.data.token);
+    // Redirect the user
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: "Invalid login credentials" });
+  }
+};
+
 export const signout = () => {
   localStorage.removeItem("token");
 
